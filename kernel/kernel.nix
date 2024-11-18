a3ea2c4622be64d13e1d3592c9c9263f64d3ecc8
Alexander Graf <graf@amazon.com>
2024-11-18 10:00:06 +0000

    kernel: Update to Linux 6.6.62
    Update Linux version to the latest LTS stable minor version 6.6.62.
    
    Signed-off-by: Alexander Graf <graf@amazon.com>

--- kernel/kernel.nix
+++ kernel/kernel.nix
@@ -36,7 +36,7 @@ let
 in
 pkgs.stdenv.mkDerivation rec {
   pname = "nitro-enclaves-kernel";
-  version = "6.6.38";
+  version = "6.6.62";
 
   nativeBuildInputs = with pkgs; [
     git
@@ -54,7 +54,7 @@ pkgs.stdenv.mkDerivation rec {
     owner = "gregkh";
     repo = "linux";
     rev = "v${version}";
-    sha256 = "sha256-DGP3preuBBEIv/PXRiSLHvEInaJ9n/hwCY7OSxvodJk=";
+    sha256 = "sha256-oWuFnRkeB6s+5C2L2pD2Ef4GXdFFsodJLbpPDmQm1sY=";
   };
 
   files = [
