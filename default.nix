51eed598bebae589ce05925ef6e728d9c6b71121
Simon Veith <sveith@amazon.de>
2024-11-20 09:59:19 +0100

    treewide: Centralize the pinned nixpkgs import
    We currently duplicate the pinned nixpkgs import in four places, for no
    apparent reason. Centralize this import into a toplevel `nixpkgs.nix`
    file.
    
    Signed-off-by: Simon Veith <sveith@amazon.de>

--- default.nix
+++ default.nix
@@ -1,8 +1,5 @@
 let
-  nixpkgs = import (fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/24.05.tar.gz";
-    sha256 = "sha256:1lr1h35prqkd1mkmzriwlpvxcb34kmhc9dnr48gkm8hh089hifmx";
-  }) { };
+  nixpkgs = (import ./nixpkgs.nix) { };
 
   arch = nixpkgs.stdenv.hostPlatform.uname.processor;
 in
