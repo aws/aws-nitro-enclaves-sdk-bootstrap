854217d81e0736b160a618bf67e38e409cdc28dc
Simon Veith <sveith@amazon.de>
2024-11-20 10:00:04 +0100

    build: Parametrize pkgs in top-level derivation
    The top-level derivation in default.nix currently always uses the
    package set pinned in nixpkgs.nix, unlike the sub-derivations which
    provide a way of overriding it.
    
    Rename the package set from `nixpkgs` to the commonly used `pkgs` and
    turn it into a parameter, which will be useful to instantiate it for
    other nixpkgs revisions or configurations (e.g. for cross-compiling).
    
    Signed-off-by: Simon Veith <sveith@amazon.de>

--- default.nix
+++ default.nix
@@ -1,17 +1,16 @@
+{ pkgs ? (import ./nixpkgs.nix) { } }:
 let
-  nixpkgs = (import ./nixpkgs.nix) { };
-
-  arch = nixpkgs.stdenv.hostPlatform.uname.processor;
+  arch = pkgs.stdenv.hostPlatform.uname.processor;
 in
 rec {
-  init = nixpkgs.callPackage ./init/init.nix { };
+  init = pkgs.callPackage ./init/init.nix { };
 
-  kernel = nixpkgs.callPackage ./kernel/kernel.nix { };
+  kernel = pkgs.callPackage ./kernel/kernel.nix { };
 
-  linuxkit = nixpkgs.callPackage ./linuxkit/linuxkit.nix { };
+  linuxkit = pkgs.callPackage ./linuxkit/linuxkit.nix { };
 
 
-  all = nixpkgs.runCommandNoCC "enclaves-blobs-${arch}" { } ''
+  all = pkgs.runCommandNoCC "enclaves-blobs-${arch}" { } ''
     mkdir -p $out/${arch}
     cp -r ${init}/* $out/${arch}/
     cp -r ${kernel}/* $out/${arch}/
